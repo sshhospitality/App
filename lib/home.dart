dcbaf3297755116997c8580394d10daa42a38bb1
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-05-18 22:49:51 +0530

    feat:notification

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,9 @@ import 'feedback_page.dart';
 import 'qr_page.dart';
 import 'menu_page.dart';
 import 'profile_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class MainHomePage extends StatefulWidget {
   const MainHomePage({super.key});
@@ -15,14 +18,7 @@ class MainHomePage extends StatefulWidget {
 class _MainHomePageState extends State<MainHomePage> {
   int _selectedIndex = 0;
   bool _isNotificationVisible = false;
-  List<String> _notifications = [
-    "Notification 1",
-    "Notification 2",
-    "Notification 3",
-    "Notification 4",
-    "Notification 5",
-    "Notification 6",
-  ];
+  List<Map<String, String>> _notifications = [];
 
   final List<Widget> _widgetOptions = <Widget>[
     const HomePage(),
@@ -39,6 +35,47 @@ class _MainHomePageState extends State<MainHomePage> {
     'Menu',
     'Profile',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotifications();
+  }
+
+  Future<void> fetchNotifications() async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String sessionCookie = prefs.getString('token') ?? 'N/A';
+      var cookie = 'token=$sessionCookie';
+      final response = await http.post(
+        Uri.parse(
+            'https://z1ogo1n55a.execute-api.ap-south-1.amazonaws.com/api/notification/get'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'cookie': cookie,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = json.decode(response.body);
+        setState(() {
+          _notifications =
+              responseData.map<Map<String, String>>((notification) {
+            return {
+              'title': notification['title'] ?? 'No title',
+              'description': notification['message'] ?? 'No description',
+              'createdAt': notification['createdAt'] ?? 'No date',
+            };
+          }).toList();
+        });
+      } else {
+        throw Exception('Failed to load notifications');
+      }
+    } catch (e) {
+      print('Error fetching notifications: $e');
+      // Handle the error appropriately in your app (e.g., show a dialog or a snackbar)
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -87,21 +124,35 @@ class _MainHomePageState extends State<MainHomePage> {
                     ),
                     Expanded(
                       child: ListView.separated(
-                        itemCount: _notifications.length,
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider(
-                              color: Colors.grey,
-                              height: 1,
-                            ),
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(
-                              _notifications[index],
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          );
-                        },
-                      ),
+                          itemCount: _notifications.length,
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(
+                                color: Colors.grey,
+                                height: 1,
+                              ),
+                          itemBuilder: (context, index) {
+                            final notification = _notifications[index];
+                            return ListTile(
+                              title: Text(
+                                notification['title'] ?? 'No title',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    notification['description'] ??
+                                        'No description',
+                                    style: TextStyle(color: Colors.black87),
+                                  ),
+                                  Text(
+                                    notification['createdAt'] ?? 'No date',
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }),
                     ),
                   ],
                 ),
