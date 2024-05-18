5022512411c475f0e19455fb78e426f1f42c82a0
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-05-18 13:03:20 +0530

    fix:merge conflict

--- lib/qr_page.dart
+++ lib/qr_page.dart
@@ -5,6 +5,8 @@ import 'package:crypto/crypto.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'home.dart';
+import 'package:intl/intl.dart';
 
 class QRPage extends StatefulWidget {
   const QRPage({super.key});
@@ -17,6 +19,7 @@ class _QRPageState extends State<QRPage> {
   String qrResult = "QR Code Result";
   final String originalText = 'naivedyam';
   late final String expectedQrCode;
+
   @override
   void initState() {
     super.initState();
@@ -26,17 +29,12 @@ class _QRPageState extends State<QRPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('QR Scanner'),
-      // ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {
-                scanQRCode();
-              },
+              onPressed: scanQRCode,
               child: const Text('Scan QR'),
             ),
             const SizedBox(height: 20.0),
@@ -50,27 +48,36 @@ class _QRPageState extends State<QRPage> {
   Future<void> postTransaction() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     try {
-      final String sessionCookie = await prefs.getString('token') ?? 'N/A';
-      final String id = await prefs.getString('_id') ?? 'N/A';
+      final String sessionCookie = prefs.getString('token') ?? 'N/A';
+      final String id = prefs.getString('_id') ?? 'N/A';
       var cookie = 'token=$sessionCookie';
-      print(id);
+
       final response = await http.post(
         Uri.parse(
             "https://z1ogo1n55a.execute-api.ap-south-1.amazonaws.com/api/txn/transactions"),
         headers: <String, String>{
           "cookie": cookie,
+          'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(<String, String>{
-          "userId": "661594aca9a498b63c75501c",
-          "transaction_mode": "QR"
-        }),
+        body: jsonEncode(
+            <String, String>{"userId": id, "transaction_mode": "QR"}),
       );
 
-      final data = json.decode(response.body);
-      print(cookie);
-      print(data);
-      showAlertDialog(context, 'Transaction Successful',
-          'Please wait as you are being redirected to transaction success page.');
+      if (response.statusCode == 201) {
+        final data = json.decode(response.body);
+        print(cookie);
+        print(data);
+        showAlertDialog(
+            context,
+            'Transaction Successful',
+            'Please wait as you are being redirected to transaction success page.',
+            data);
+      } else {
+        final data = json.decode(response.body);
+        print(data);
+        showAlertDialog(
+            context, 'Transaction Failed', data['message'] ?? 'Unknown error');
+      }
     } catch (e) {
       showAlertDialog(context, 'Transaction Failed', '$e');
     }
@@ -97,7 +104,6 @@ class _QRPageState extends State<QRPage> {
           qrResult = 'Correct QR Code! Proceeding with API call.';
         });
       } else if (qrCode != "-1") {
-        // Show alert dialog
         showAlertDialog(
             context, 'Incorrect QR Code', 'Please scan the correct QR code.');
       }
@@ -110,7 +116,8 @@ class _QRPageState extends State<QRPage> {
     }
   }
 
-  void showAlertDialog(BuildContext context, String title, String message) {
+  void showAlertDialog(BuildContext context, String title, String message,
+      [Map<String, dynamic>? data]) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -122,11 +129,99 @@ class _QRPageState extends State<QRPage> {
               child: const Text('OK'),
               onPressed: () {
                 Navigator.of(context).pop();
+                if (data != null) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            TransactionSuccessPage(data: data)),
+                  );
+                }
               },
             ),
           ],
         );
       },
+    );
+  }
+}
+
+class TransactionSuccessPage extends StatelessWidget {
+  final Map<String, dynamic> data;
+
+  TransactionSuccessPage({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    final transaction = data['transaction'];
+    final timestamp = DateTime.parse(transaction['timestamp']);
+    final formattedTimestamp =
+        DateFormat('yyyy-MM-dd – kk:mm').format(timestamp);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transaction Success'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                'Transaction was successful!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Transaction Details:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            _buildDetailRow('Account From:', transaction['account_from']),
+            _buildDetailRow('Account To:', transaction['account_to']),
+            _buildDetailRow('Meal Type:', transaction['mealType']),
+            _buildDetailRow(
+                'Transaction Mode:', transaction['transaction_mode']),
+            _buildDetailRow('Transaction Ref No:',
+                transaction['transaction_ref_no'].toString()),
+            _buildDetailRow(
+                'Meal Items:', transaction['meal_items'].join(', ')),
+            _buildDetailRow('Transaction ID:', transaction['_id'].toString()),
+            _buildDetailRow('Timestamp:', formattedTimestamp),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                        builder: (context) => const MainHomePage()),
+                  );
+                },
+                child: const Text('Go to Home Page'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailRow(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$title ',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: Text(value),
+          ),
+        ],
+      ),
     );
   }
 }
