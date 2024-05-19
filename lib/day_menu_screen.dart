ca9158f588fa876adcb1447a69fee2e939126fbc
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2024-05-19 23:48:22 +0530

    shimmer added to menu screens

--- lib/day_menu_screen.dart
+++ lib/day_menu_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'shimmer.dart'; // Import the shimmer.dart file
 
-class DayMenuScreen extends StatelessWidget {
+class DayMenuScreen extends StatefulWidget {
   final String day;
   final dynamic menuData; // Use the appropriate type for menu data
 
   DayMenuScreen({required this.day, required this.menuData});
+
+  @override
+  _DayMenuScreenState createState() => _DayMenuScreenState();
+}
+
+class _DayMenuScreenState extends State<DayMenuScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMenuData();
+  }
+
+  Future<void> _loadMenuData() async {
+    // Simulate network delay
+    await Future.delayed(Duration(seconds: 2));
+
+    // Set loading to false after data is loaded
+    setState(() {
+      isLoading = false;
+    });
+  }
 
   // Function to determine the correct image based on meal type and category
   String getImage(String mealType, String category) {
@@ -36,108 +60,110 @@ class DayMenuScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('$day\'s Menu'),
+        title: Text('${widget.day}\'s Menu'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: menuData['meals'].map<Widget>((meal) {
-          final String mealType = meal['type'];
-          final List<dynamic> items = meal['items'];
+      body: isLoading
+          ? Center(child: ShimmerLoading()) // Show shimmer loading while data is loading
+          : ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: widget.menuData['meals'].map<Widget>((meal) {
+                final String mealType = meal['type'];
+                final List<dynamic> items = meal['items'];
 
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  mealType,
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple),
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: items.map<Widget>((item) {
-                      return Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 160,
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16)),
-                              elevation: 5,
-                              child: Stack(
-                                children: [
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.vertical(
-                                              top: Radius.circular(16)),
-                                          child: Image.asset(
-                                            getImage(
-                                                mealType, item['category']),
-                                            width: double.infinity,
-                                            fit: BoxFit.cover,
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        mealType,
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepPurple),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: items.map<Widget>((item) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 10),
+                              child: SizedBox(
+                                width: 160,
+                                child: AspectRatio(
+                                  aspectRatio: 1,
+                                  child: Card(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(16)),
+                                    elevation: 5,
+                                    child: Stack(
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Expanded(
+                                              child: ClipRRect(
+                                                borderRadius: BorderRadius.vertical(
+                                                    top: Radius.circular(16)),
+                                                child: Image.asset(
+                                                  getImage(
+                                                      mealType, item['category']),
+                                                  width: double.infinity,
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                item['name'],
+                                                style: TextStyle(
+                                                    fontSize: 14, color: Colors.grey),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Positioned(
+                                          bottom: 0,
+                                          right: 0,
+                                          child: Container(
+                                            padding: EdgeInsets.all(4),
+                                            decoration: BoxDecoration(
+                                              color: item['category'] == 'Veg'
+                                                  ? Colors.green
+                                                  : Colors.red,
+                                              borderRadius: BorderRadius.only(
+                                                topLeft: Radius.circular(8),
+                                              ),
+                                            ),
+                                            child: Icon(
+                                              item['category'] == 'Veg'
+                                                  ? Icons.eco
+                                                  : Icons.auto_awesome,
+                                              color: Colors.white,
+                                              size: 18,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          item['name'],
-                                          style: TextStyle(
-                                              fontSize: 14, color: Colors.grey),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Positioned(
-                                    bottom: 0,
-                                    right: 0,
-                                    child: Container(
-                                      padding: EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                        color: item['category'] == 'Veg'
-                                            ? Colors.green
-                                            : Colors.red,
-                                        borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(8),
-                                        ),
-                                      ),
-                                      child: Icon(
-                                        item['category'] == 'Veg'
-                                            ? Icons.eco
-                                            : Icons.auto_awesome,
-                                        color: Colors.white,
-                                        size: 18,
-                                      ),
+                                      ],
                                     ),
                                   ),
-                                ],
+                                ),
                               ),
-                            ),
-                          ),
+                            );
+                          }).toList(),
                         ),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20), // Add some space between sections
-            ],
-          );
-        }).toList(),
-      ),
+                      ),
+                    ),
+                    const SizedBox(height: 20), // Add some space between sections
+                  ],
+                );
+              }).toList(),
+            ),
     );
   }
 }
